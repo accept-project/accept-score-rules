770a6478e751914d9551975eb8536fc0f48ebc10
Robert Grabowski <robert.grabowski@acrolinx.com>
2012-03-19 11:12:44 +0000

    (rg) - improved BLEU scorer: added smoothing, sentence-by-sentence scorability - made it usable for very short sentences

--- bleu/src/lingutil/bleu/BleuMeasurer.java
+++ bleu/src/lingutil/bleu/BleuMeasurer.java
@@ -1,34 +1,27 @@
 /*
- *  Copyright (c) 2009 Ondrej Dusek
- *  All rights reserved.
+ * Copyright (c) 2009 Ondrej Dusek All rights reserved.
  * 
- *  Redistribution and use in source and binary forms, with or without modification, 
- *  are permitted provided that the following conditions are met:
- *  Redistributions of source code must retain the above copyright notice, this list 
- *  of conditions and the following disclaimer.
- *  Redistributions in binary form must reproduce the above copyright notice, this 
- *  list of conditions and the following disclaimer in the documentation and/or other 
- *  materials provided with the distribution.
- *  Neither the name of Ondrej Dusek nor the names of their contributors may be
- *  used to endorse or promote products derived from this software without specific 
- *  prior written permission.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met: Redistributions of source code must retain the
+ * above copyright notice, this list of conditions and the following disclaimer. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution. Neither
+ * the name of Ondrej Dusek nor the names of their contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
  * 
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
- *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /**
  * @file BleuMeasurer.java An implementation of the BLEU measure.
  * @author Ondřej Dušek
  */
-
 
 package lingutil.bleu;
 
@@ -38,7 +31,8 @@ import java.util.Hashtable;
 /**
  * An implementation of the BLEU measure.
  */
-public class BleuMeasurer {
+public class BleuMeasurer
+{
 
     /* CONSTANTS */
 
@@ -46,41 +40,41 @@ public class BleuMeasurer {
     private static final int MAX_NGRAM = 4;
 
     /* FIELDS */
-    
+
     /** Number of clipped hits throughout the corpus */
-    private int clippedHits [];
+    private int clippedNgramHits[];
     /** Number of n-grams in the candidate corpus */
-    private int candLenghts [];
+    private int candNgramCount[];
 
     /** Length of the reference corpus */
     private int refLength;
-
 
     /* METHODS */
 
     /**
      * Constructor, just inits (zeroes) everything.
      */
-    public BleuMeasurer(){
+    public BleuMeasurer()
+    {
 
-        this.candLenghts = new int [MAX_NGRAM];
-        this.clippedHits = new int [MAX_NGRAM];
+        this.candNgramCount = new int[MAX_NGRAM];
+        this.clippedNgramHits = new int[MAX_NGRAM];
     }
 
     /**
-     * Adds a sentence to the considered corpus (the statistics are
-     * summed up).
-     *
+     * Adds a sentence to the considered corpus (the statistics are summed up).
+     * 
      * @param refTokens the reference translation
      * @param candTokens the candidate translation
      */
-    public void addSentence(String [] refTokens, String [] candTokens){
+    public void addSentence(String[] refTokens, String[] candTokens)
+    {
 
-        for (int i = 1; i <= MAX_NGRAM; ++i){
+        for (int i = 1; i <= MAX_NGRAM; ++i) {
             // adds clipped hits to stats for corpus
             saveClippedHits(i, refTokens, candTokens);
             // adds total n-gram candidate length to stats for corpus
-            this.candLenghts[i-1] += candTokens.length - i + 1;
+            this.candNgramCount[i - 1] += Math.max(candTokens.length - i + 1, 0);
         }
 
         // adds reference lenght to stats for corpus
@@ -89,34 +83,42 @@ public class BleuMeasurer {
 
     /**
      * Resets the currently computed statistics to zero.
-     *
+     * 
      */
-    public void reset(){
+    public void reset()
+    {
 
         this.refLength = 0;
-        this.candLenghts = new int [MAX_NGRAM];
-        this.clippedHits = new int [MAX_NGRAM];
+        this.candNgramCount = new int[MAX_NGRAM];
+        this.clippedNgramHits = new int[MAX_NGRAM];
     }
 
-
     /**
-     * Returns the current BLEU score, according to the stored
-     * statistics.
-     *
+     * Returns the current BLEU score, according to the stored statistics.
+     * 
      * @return the current BLEU score
      */
-    public double bleu(){
+    public double bleu()
+    {
 
         double bp = 1.0; // brevity penalty
         double precAvg = 0.0; // modified n-gram precisions
         double bleu;
 
-        if (this.candLenghts[0] <= this.refLength){
-            bp = Math.exp(1.0 - this.refLength/this.candLenghts[0]);
+        if (this.candNgramCount[0] <= this.refLength) {
+            bp = Math.exp(1.0 - this.refLength / this.candNgramCount[0]);
         }
 
-        for (int i = 0; i < MAX_NGRAM; ++i){
-            precAvg += (1.0/MAX_NGRAM) * Math.log(this.clippedHits[i] / (double)this.candLenghts[i]);
+        for (int i = 0; i < MAX_NGRAM; ++i) {
+            if (i == 0) {
+                precAvg += (1.0 / MAX_NGRAM) * Math.log(this.clippedNgramHits[i] / (double) this.candNgramCount[i]);
+            } else {
+                // use smoothed precision average for n-grams with n>1:
+                // add 1 to n-gram count and hits to prevent BLEU score from becoming 0 if no n-gram
+                // matches.
+                precAvg += (1.0 / MAX_NGRAM) * Math.log((this.clippedNgramHits[i] + 1) / ((double) (this.candNgramCount[i]) + 1));
+            }
+
         }
 
         bleu = bp * Math.exp(precAvg);
@@ -125,58 +127,60 @@ public class BleuMeasurer {
 
     /**
      * Saves the clipped hits for a sentence to the total statistics.
-     *
+     * 
      * @param nGram the n-gram setting (1 to 4)
      * @param refTokens the reference translation
-     * @param candTokens  the candidate translation
+     * @param candTokens the candidate translation
      */
-    private void saveClippedHits(int nGram, String [] refTokens, String [] candTokens) {
+    private void saveClippedHits(int nGram, String[] refTokens, String[] candTokens)
+    {
 
-        Hashtable<String,Integer> candStats, refStats;
+        Hashtable<String, Integer> candStats, refStats;
 
         candStats = collectStatistics(candTokens, nGram);
         refStats = collectStatistics(refTokens, nGram);
 
         Enumeration<String> candNGrams = candStats.keys();
-        while (candNGrams.hasMoreElements()){
+        while (candNGrams.hasMoreElements()) {
 
             String curNGram = candNGrams.nextElement();
             int candCnt, refCnt = 0;
 
             candCnt = candStats.get(curNGram);
 
-            if (refStats.containsKey(curNGram)){
+            if (refStats.containsKey(curNGram)) {
                 refCnt = refStats.get(curNGram);
             }
 
-            this.clippedHits[nGram-1] += Math.min(candCnt, refCnt);
+            this.clippedNgramHits[nGram - 1] += Math.min(candCnt, refCnt);
         }
     }
 
     /**
-     * Collects statistics for given n-gram and a string (used for computing
-     * modified n-gram precision)
+     * Collects statistics for given n-gram and a string (used for computing modified n-gram
+     * precision)
+     * 
      * @param tokens the string whose statistics are to be computed
      * @param nGram n-gram setting
-     * @return the statistics -- for each possible n-gram, there is a key in the hashtable, and
-     *        the number of occurrences is the value
+     * @return the statistics -- for each possible n-gram, there is a key in the hashtable, and the
+     *         number of occurrences is the value
      */
-    private static Hashtable<String,Integer> collectStatistics(String [] tokens, int nGram){
+    private static Hashtable<String, Integer> collectStatistics(String[] tokens, int nGram)
+    {
 
-        Hashtable<String,Integer> stats = new Hashtable<String,Integer>();
+        Hashtable<String, Integer> stats = new Hashtable<String, Integer>();
 
-        for (int i = 0; i < tokens.length; ++i){
+        for (int i = 0; i < tokens.length; ++i) {
 
             String nGramString = makeNGram(tokens, i, nGram);
 
-            if (nGramString == null){
+            if (nGramString == null) {
                 break;
             }
 
-            if (stats.containsKey(nGramString)){
+            if (stats.containsKey(nGramString)) {
                 stats.put(nGramString, stats.get(nGramString) + 1);
-            }
-            else {
+            } else {
                 stats.put(nGramString, 1);
             }
         }
@@ -184,25 +188,26 @@ public class BleuMeasurer {
     }
 
     /**
-     * Composes an n-gram out of the first nGram tokens from the position off in the
-     * candTokens array. Returns null if the desired nGram is out of range.
-     *
+     * Composes an n-gram out of the first nGram tokens from the position off in the candTokens
+     * array. Returns null if the desired nGram is out of range.
+     * 
      * @param candTokens array where to get the tokens from
      * @param off starting position
      * @param nGram the n-gram setting (how long an n-gram to make)
      * @returns the desired n-gram or null if out of range
      */
-    private static String makeNGram(String[] candTokens, int off, int nGram) {
+    private static String makeNGram(String[] candTokens, int off, int nGram)
+    {
 
         StringBuilder sb;
 
-        if (off + nGram > candTokens.length){
+        if (off + nGram > candTokens.length) {
             return null;
         }
 
         sb = new StringBuilder(candTokens[off]);
 
-        for (int j = off + 1; j < off + nGram; ++j){
+        for (int j = off + 1; j < off + nGram; ++j) {
             sb.append(' ');
             sb.append(candTokens[j]);
         }
--- bleu/src/lingutil/bleu/Main.java
+++ bleu/src/lingutil/bleu/Main.java
@@ -1,28 +1,22 @@
 /*
- *  Copyright (c) 2009 Ondrej Dusek
- *  All rights reserved.
+ * Copyright (c) 2009 Ondrej Dusek All rights reserved.
  * 
- *  Redistribution and use in source and binary forms, with or without modification, 
- *  are permitted provided that the following conditions are met:
- *  Redistributions of source code must retain the above copyright notice, this list 
- *  of conditions and the following disclaimer.
- *  Redistributions in binary form must reproduce the above copyright notice, this 
- *  list of conditions and the following disclaimer in the documentation and/or other 
- *  materials provided with the distribution.
- *  Neither the name of Ondrej Dusek nor the names of their contributors may be
- *  used to endorse or promote products derived from this software without specific 
- *  prior written permission.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met: Redistributions of source code must retain the
+ * above copyright notice, this list of conditions and the following disclaimer. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution. Neither
+ * the name of Ondrej Dusek nor the names of their contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
  * 
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
- *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /**
  * @file Main.java The main class, reading command arguments and input.
@@ -35,27 +29,26 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * The main class, reading command arguments and input.
  */
-public class Main {
+public class Main
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         BleuMeasurer bm;
         BufferedReader inRef = null, inCand = null;
-        boolean eof = false;
-        int lineCtr = 0;
+        // int lineCtr = 0;
 
         // parameters check
-	//        if ((args.length == 1) && "--".equals(args[0])) {
-        
-        //}
-        if (args.length != 2){
+        if (!((args.length == 1) && "--".equals(args[0])) && (args.length != 2)) {
             System.err.println("Input parameters: reference_file candidate_file");
             System.exit(1);
         }
@@ -64,57 +57,64 @@ public class Main {
         bm = new BleuMeasurer();
 
         try {
-            inRef = new BufferedReader(new FileReader(args[0]));
-            inCand = new BufferedReader(new FileReader(args[1]));
-        }
-        catch(FileNotFoundException e){
+            if (args.length == 1) {
+                inRef = new BufferedReader(new InputStreamReader(System.in));
+                inCand = inRef;
+            } else {
+                inRef = new BufferedReader(new FileReader(args[0]));
+                inCand = new BufferedReader(new FileReader(args[1]));
+            }
+        } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
             System.exit(2);
         }
 
         // read sentence by sentence
-        while (!eof){
+        while (true) {
 
             String candLine = null, refLine = null;
-            String [] candTokens;
-            String [] refTokens;
+            String[] candTokens;
+            String[] refTokens;
 
             try {
                 refLine = inRef.readLine();
                 candLine = inCand.readLine();
-            }
-            catch (IOException ex) {
+            } catch (IOException ex) {
                 System.err.println(ex.getMessage());
                 System.exit(1);
             }
 
             // test for EOF
-            if (candLine == null && refLine == null){
+            if (candLine == null && refLine == null) {
                 break;
             }
-            if (candLine == null || refLine == null){
+            if (candLine == null || refLine == null) {
                 System.err.println("The files are of different lengths.");
                 System.exit(1);
             }
 
             // split to tokens by whitespace
-            candLine.trim(); refLine.trim();
+            candLine.trim();
+            refLine.trim();
             candTokens = candLine.split("\\s+");
             refTokens = refLine.split("\\s+");
 
             // add sentence to stats
             bm.addSentence(refTokens, candTokens);
-            if (lineCtr % 100 == 0){
-                System.err.print(".");
-            }
-	    System.out.println("Sentence: " + lineCtr + ", score: " + bm.bleu());
-	    bm.reset();
-            lineCtr++;
+            // if (lineCtr % 100 == 0) {
+            // System.err.print(".");
+            // }
+            // System.out.println("Sentence: " + lineCtr + ", score: " + bm.bleu());
+
+            System.out.println(bm.bleu());
+            bm.reset();
+
+            // lineCtr++;
         }
 
         // print the result
-        //System.err.println("Total:" + lineCtr + " sentences.");
-        //System.out.println("BLEU score: " + bm.bleu());
+        // System.err.println("Total:" + lineCtr + " sentences.");
+        // System.out.println("BLEU score: " + bm.bleu());
     }
 
 }
